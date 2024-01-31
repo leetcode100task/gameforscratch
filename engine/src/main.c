03c1157cb9f9dce6ee154fccbef7979b4346b6ef
Stepan <stepantryaschin@mail.ru>
2024-01-31 20:11:26 +0400

    baze window

--- engine/src/main.c
+++ engine/src/main.c
@@ -1,13 +1,53 @@
-#include<stdio.h>
-#include<stdbool.h>
-#include<glad/glad.h>
+#include <stdio.h>
+#include <stdbool.h>
 #define SDL_MAIN_HANDLED
-#include<SDL2/SDL.h>
-int main(int argc, char*argv[]){
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	puts("Hello");
-	return 0;
-	
+#include <SDL2/SDL.h>
+#include <glad/glad.h>
+
+int main() {
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL initialization failed: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SDL2 Starter Project",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_OPENGL);
+
+    if (!window) {
+        printf("SDL window creation failed: %s\n", SDL_GetError());
+        exit(1);
+    }
+
+    SDL_GL_CreateContext(window);
+
+    if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) {
+        printf("GLAD initialization failed\n");
+        exit(1);
+    }
+
+    puts("OpenGL loaded");
+    printf("Vendor: %s\n", glGetString(GL_VENDOR));
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("Version: %s\n", glGetString(GL_VERSION));
+    bool quit = false;
+    while (!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                quit = true;
+                break;
+            }
+        }
+        // Your game logic goes here
+    }
+    SDL_GL_DeleteContext(window);
+    SDL_Quit();
+
+    return 0;
 }
